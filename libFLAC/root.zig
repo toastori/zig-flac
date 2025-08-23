202ac9124474bdd75a1fc3e02fd51c7c687f2b59
toastori <valbian@gmail.com>
2025-08-23 23:49:54 +0800

    remove unremoved iterators from libFLAC/root.zig

--- libFLAC/root.zig
+++ libFLAC/root.zig
@@ -8,7 +8,3 @@ pub const metadata = @import("metadata.zig");
 // -- Types --
 pub const Encoder = @import("FlacEncoder.zig");
 pub const FrameWriter = @import("FrameWriter.zig");
-
-pub const SampleIter = sample_iter.SampleIter;
-pub const MultiChannelIter = sample_iter.MultiChannelIter;
-pub const SingleChannelIter = sample_iter.SingleChannelIter;

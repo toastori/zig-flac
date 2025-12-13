52a443f3ed5701af1f06d9c95277883920dba7be
toastori <valbian@gmail.com>
2025-12-14 01:33:12 +0800

    fix typo in build.zig

--- build.zig
+++ build.zig
@@ -58,7 +58,7 @@ pub fn build(b: *std.Build) void {
     });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    const test_step = b.step(".test", "Run unit tests");
+    const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 
     // Check Step (no emit bin)

5e64b7a09532d5823e6fddfdc8da421d17cf1594
toastori <valbian@gmail.com>
2026-03-15 00:14:48 +0800

    rename FlacEncoder to Encoder

--- libFLAC/root.zig
+++ libFLAC/root.zig
@@ -6,5 +6,5 @@ const sample_iter = @import("samples.zig");
 pub const metadata = @import("metadata.zig");
 
 // -- Types --
-pub const Encoder = @import("FlacEncoder.zig");
+pub const Encoder = @import("Encoder.zig");
 pub const FrameWriter = @import("FrameWriter.zig");
